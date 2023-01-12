34f8ffa8d2484e46843ea79f1c4c88fbad4a16a8
Bertissolo <wesley.bertissolo@gmail.com>
2023-01-12 14:02:27 -0300

    pagina de login completa

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,8 +1,58 @@
+import { useState } from 'react';
+
+import all4conn from './assets/ALL4CONN.jpg'
+
+import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
-    <h1>Hello word!</h1>
-  );
+    <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form">
+              <span className="login-form-title">All4Conn!</span>
+              <span className="login-form-title">
+                <img src={all4conn} alt="All4Conn Tecnologia" />
+              </span>
+
+              <div className="wrap-input">
+                <input 
+                className={email !== "" ? 'has-val input' : 'input'} 
+                type="email" 
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Email"></span>
+              </div>
+
+              <div className="wrap-input">
+                <input 
+                className={password !== "" ? 'has-val input' : 'input'}  
+                type="password" 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Password"></span>
+              </div>
+
+              <div className="container-login-form-btn">
+                <button className="login-form-btn">Login</button>
+              </div>
+
+              <div className="text-center">
+                <span className="txt1">Não possui conta?</span>
+                
+                <a className="txt2" href="#">Criar conta.</a>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
